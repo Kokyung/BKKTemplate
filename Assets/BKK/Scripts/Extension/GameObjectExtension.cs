54faa94fea4a3566f149f124caaf87c9983c727e
Kokyung <43735316+Kokyung@users.noreply.github.com>
2022-10-13 02:45:47 +0900

    게임오브젝트 유틸리티 기능 추가

--- Assets/BKK/Scripts/Extension/GameObjectExtension.cs
+++ Assets/BKK/Scripts/Extension/GameObjectExtension.cs
@@ -50,6 +50,117 @@ namespace BKK.Extension
                 return true;
             }
         }
+
+        public static void SetChildren(this Transform target, Transform[] children)
+        {
+            foreach (var child in children)
+            {
+                target.SetChild(child);
+            }
+        }
+
+        public static void SetChild(this Transform target, Transform child)
+        {
+            child.SetParent(target);
+        }
+
+        public static Vector3 InsertX(this Vector3 pos, float value)
+        {
+            pos.x = value;
+            return pos;
+        }
+        
+        public static Vector3 InsertY(this Vector3 pos, float value)
+        {
+            pos.y = value;
+            return pos;
+        }
+        
+        public static Vector3 InsertZ(this Vector3 pos, float value)
+        {
+            pos.z = value;
+            return pos;
+        }
+        
+        public static Vector3 InsertXY(this Vector3 pos, float x, float y)
+        {
+            pos.x = x;
+            pos.y = y;
+            return pos;
+        }
+        
+        public static Vector3 InsertYZ(this Vector3 pos, float y, float z)
+        {
+            pos.y = y;
+            pos.z = z;
+            return pos;
+        }
+        
+        public static Vector3 InsertXZ(this Vector3 pos, float x, float z)
+        {
+            pos.x = x;
+            pos.z = z;
+            return pos;
+        }
+
+        public static Transform InsertX(this Transform tr, float value)
+        {
+            tr.position = tr.position.InsertX(value);
+            return tr;
+        }
+        
+        public static Transform InsertY(this Transform tr, float value)
+        {
+            tr.position = tr.position.InsertY(value);
+            return tr;
+        }
+        
+        public static Transform InsertZ(this Transform tr, float value)
+        {
+            tr.position = tr.position.InsertZ(value);
+            return tr;
+        }
+        
+        public static Transform InsertXY(this Transform tr, float x, float y)
+        {
+            tr.position = tr.position.InsertXY(x, y);
+            return tr;
+        }
+        
+        public static Transform InsertYZ(this Transform tr, float y, float z)
+        {
+            tr.position = tr.position.InsertYZ(y, z);
+            return tr;
+        }
+        
+        public static Transform InsertXZ(this Transform tr, float x, float z)
+        {
+            tr.position = tr.position.InsertXZ(x, z);
+            return tr;
+        }
+
+
+
+
+        public static bool ContainsScreenPoint(this RectTransform rectTransform, Vector3 pos)
+        {
+            return RectTransformUtility.RectangleContainsScreenPoint(rectTransform, pos);
+        }
+        
+        public static bool ContainsScreenPoint(this RectTransform rectTransform, Vector3 pos, Camera eventCamera)
+        {
+            return RectTransformUtility.RectangleContainsScreenPoint(rectTransform, pos, eventCamera);
+        }
+        
+        public static bool ContainsScreenPoint(this RectTransform rectTransform, Vector3 pos, Camera eventCamera, Vector4 offset)
+        {
+            return RectTransformUtility.RectangleContainsScreenPoint(rectTransform, pos, eventCamera, offset);
+        }
+        
+        public static Vector2 WorldToScreenPoint(this Vector3 worldPoint, Camera eventCamera)
+        {
+            return RectTransformUtility.WorldToScreenPoint(eventCamera, worldPoint);
+        }
     }
 }
 
