86aa088c3c3d86119b355d29c238e67b2a97b27d
Kokyung <43735316+Kokyung@users.noreply.github.com>
2022-10-14 20:48:10 +0900

    빌드 알림음 추가

--- Assets/BKK/Editor/PostBuildProcessor.cs
+++ Assets/BKK/Editor/PostBuildProcessor.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using BKK.EditorCustomUtility;
 using UnityEngine;
 using UnityEditor;
@@ -8,7 +10,15 @@ using UnityEditor.Callbacks;
 public class PostBuildProcessor : MonoBehaviour
 {
     [PostProcessBuild(1)]
-    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
-        Debug.Log( pathToBuiltProject );
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        Debug.Log(pathToBuiltProject);
+        BuildCompleteSound();
+    }
+
+    private static async void BuildCompleteSound()
+    {
+        await Task.Delay(2000);
+        EditorSFX.PlayClip(Resources.Load<AudioClip>("BuildComplete-HyunJu"));
     }
 }
