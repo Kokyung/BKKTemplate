e97e1e10f948192013ddd491602169c6e912c199
Kokyung <43735316+Kokyung@users.noreply.github.com>
2023-01-10 00:09:21 +0900

    Unity용 Azure Blob Storage Service 추가

---
+++ Assets/AzureBlobStorageService/BKK/AzureBlobStorageService.cs
@@ -0,0 +1,483 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.StorageServices;
+using Cysharp.Threading.Tasks;
+using RESTClient;
+using UnityEngine;
+
+namespace Azure.Custom
+{
+    /// <summary>
+    /// Azure Blob Storage에 데이터를 업로드/다운로드할 수 있게 해주는 컴포넌트
+    ///
+    /// 다음 링크 내용을 참고하였습니다.
+    /// https://answers.unity.com/questions/1833982/how-to-get-a-text-file-video-from-azure-blob-stora.html
+    /// </summary>
+    public class AzureBlobStorageService : MonoBehaviour
+    {
+        [Tooltip("스토리지 계정 이름.\nAzure Portal에서 해당 [스토리지 계정 -> 엑세스 키]에서 확인 가능합니다.")]
+        [SerializeField] private string storageAccountName = "BKK";
+        [Tooltip("스토리지 계정의 엑세스 키.\nAzure Portal에서 해당 [스토리지 계정 -> 엑세스 키]에서 확인 가능합니다.")]
+        [SerializeField] private string accessKey = "";
+
+        [Tooltip("기본 컨테이너 경로")]
+        [SerializeField] private string defaultContainerPath = "Container/Cache";
+        [Tooltip("비디오 컨테이너 경로")]
+        [SerializeField] private string videoContainerPath = "Container/Cache/Video";
+        [Tooltip("오디오 컨테이너 경로")]
+        [SerializeField] private string audioContainerPath = "Container/Cache/Audio";
+        [Tooltip("이미지 컨테이너 경로")]
+        [SerializeField] private string imageContainerPath = "Container/Cache/Image";
+
+        private StorageServiceClient client;
+        private BlobService blobService;
+        private FilePicker filePicker;
+
+        #region Debug
+
+        private string tempFileName;
+        private string tempPath;
+
+        [Tooltip("모바일 디버깅용 GUI 활성화 여부")]
+        [SerializeField] private bool debug = false;
+
+        #endregion
+
+        #region Monobehaviour
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnGUI()
+        {
+            if(!debug) return;
+            
+            if (GUI.Button(new Rect(50, 50, 100, 40), "비디오 업로드"))
+            {
+                PickAndUploadVideoFile();
+            }
+            if (GUI.Button(new Rect(50, 100, 100, 40), "오디오 업로드"))
+            {
+                PickAndUploadAudioFile();
+            }
+            if (GUI.Button(new Rect(50, 150, 100, 40), "이미지 업로드"))
+            {
+                PickAndUploadImageFile();
+            }
+            if (GUI.Button(new Rect(50, 200, 200, 40), "직전 업로드된 파일 다운로드"))
+            {
+                DownloadFileDemo();
+            }
+            if (GUI.Button(new Rect(50, 250, 200, 40), "직전 업로드된 파일 삭제"))
+            {
+                DeleteVideoFileDemo();
+            }
+        }
+
+        #endregion
+
+        #region Initialize
+
+        private void Init()
+        {
+            client = StorageServiceClient.Create(storageAccountName, accessKey);
+            blobService = client.GetBlobService();
+
+            filePicker = FindObjectOfType<FilePicker>();
+
+            if (!filePicker) filePicker = this.gameObject.AddComponent<FilePicker>();
+        }
+
+        #endregion
+
+        #region Main Function
+        
+        /// <summary>
+        /// 파일 폴더에서 선택한 비디오 파일을 업로드한다.
+        /// </summary>
+        public void PickAndUploadVideoFile()
+        {
+            filePicker.PickVideoFile(async delegate(string[] paths)
+            {
+                foreach (var path in paths)
+                {
+                    var fileName = path.Substring(path.LastIndexOf('/') + 1);
+                    var fileFormat = fileName.Substring(fileName.LastIndexOf('.') + 1).ToLower();
+                    await UploadVideoFile(path, defaultContainerPath, fileName, fileFormat);
+                    
+                    tempFileName = fileName;
+                    tempPath = defaultContainerPath;
+                }
+            });
+        }
+        
+        /// <summary>
+        /// 파일 폴더에서 선택한 오디오 파일을 업로드한다.
+        /// </summary>
+        public void PickAndUploadAudioFile()
+        {
+            filePicker.PickAudioFile(async delegate(string[] paths)
+            {
+                foreach (var path in paths)
+                {
+                    var fileName = path.Substring(path.LastIndexOf('/') + 1);
+                    var fileFormat = fileName.Substring(fileName.LastIndexOf('.') + 1).ToLower();
+                    await UploadAudioFile(path, audioContainerPath, fileName, fileFormat);
+                    
+                    tempFileName = fileName;
+                    tempPath = audioContainerPath;
+                }
+            });
+        }
+        
+        /// <summary>
+        /// 파일 폴더에서 선택한 이미지 파일을 업로드한다.
+        /// </summary>
+        public void PickAndUploadImageFile()
+        {
+            filePicker.PickImageFile(async delegate(string[] paths)
+            {
+                foreach (var path in paths)
+                {
+                    var fileName = path.Substring(path.LastIndexOf('/') + 1);
+                    var fileFormat = fileName.Substring(fileName.LastIndexOf('.') + 1).ToLower();
+                    await UploadImageFile(path, imageContainerPath, fileName, fileFormat);
+                    
+                    tempFileName = fileName;
+                    tempPath = imageContainerPath;
+                }
+            });
+        }
+        
+        /// <summary>
+        /// 컨테이너에 비디오 파일을 다운로드한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        public async void DownloadVideoFile(string fileName)
+        {
+            var blobFilePath = $"{videoContainerPath}/{fileName}";
+
+            await DownloadVideoFile(delegate(IRestResponse<byte[]> response)
+            {
+                if (!response.IsError)
+                {
+                    var path = $"{Application.persistentDataPath}/{blobFilePath}";
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    File.WriteAllBytes($"{Application.persistentDataPath}/{blobFilePath}", response.Data);
+                }
+            }, blobFilePath);
+        }
+        
+        /// <summary>
+        /// 컨테이너에 오디오 파일을 다운로드한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        /// <param name="afterAction">다운로드 후 액션</param>
+        public async void DownloadAudioFile(string fileName, Action<IRestResponse<AudioClip>> afterAction)
+        {
+            var blobFilePath = $"{audioContainerPath}/{fileName}";
+
+            await DownloadAudioFile(afterAction, blobFilePath);
+        }
+        
+        /// <summary>
+        /// 컨테이너에 이미지 파일을 다운로드한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        /// <param name="afterAction">다운로드 후 액션</param>
+        public async void DownloadImageFile(string fileName, Action<IRestResponse<Texture>> afterAction)
+        {
+            var blobFilePath = $"{imageContainerPath}/{fileName}";
+
+            await DownloadImageFile(afterAction, blobFilePath);
+        }
+
+        /// <summary>
+        /// 컨테이너의 비디오 파일을 삭제한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        public void DeleteRemoteVideoFile(string fileName)
+        {
+            DeleteFile(videoContainerPath, fileName);
+        }
+        
+        /// <summary>
+        /// 컨테이너의 오디오 파일을 삭제한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        public void DeleteRemoteAudioFile(string fileName)
+        {
+            DeleteFile(audioContainerPath, fileName);
+        }
+        
+        /// <summary>
+        /// 컨테이너의 이미지 파일을 삭제한다.
+        /// </summary>
+        /// <param name="fileName"></param>
+        public void DeleteRemoteImageFile(string fileName)
+        {
+            DeleteFile(imageContainerPath, fileName);
+        }
+        
+        /// <summary>
+        /// 파일의 경로를 리턴한다.
+        /// </summary>
+        /// <param name="containerPath">컨테이너 경로</param>
+        /// <param name="fileName">파일명</param>
+        /// <returns></returns>
+        public string GetFileUrl(string containerPath, string fileName)
+        {
+            return $"https://{storageAccountName}.blob.core.windows.net/{containerPath}/{fileName}";
+        }
+        
+        /// <summary>
+        /// 기본 컨테이너에 있는 파일 경로를 리턴한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        /// <returns></returns>
+        public string GetFileUrl(string fileName)
+        {
+            return GetFileUrl(defaultContainerPath, fileName);
+        }
+        
+        /// <summary>
+        /// 비디오 컨테이너에 있는 파일 경로를 리턴한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        /// <returns></returns>
+        public string GetVideoFileUrl(string fileName)
+        {
+            return GetFileUrl(videoContainerPath, fileName);
+        }
+        
+        /// <summary>
+        /// 오디오 컨테이너에 있는 파일 경로를 리턴한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        /// <returns></returns>
+        public string GetAudioFileUrl(string fileName)
+        {
+            return GetFileUrl(audioContainerPath, fileName);
+        }
+        
+        /// <summary>
+        /// 이미지 컨테이너에 있는 파일 경로를 리턴한다.
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        /// <returns></returns>
+        public string GetImageFileUrl(string fileName)
+        {
+            return GetFileUrl(imageContainerPath, fileName);
+        }
+
+        #endregion
+        
+        #region Upload
+
+        public async Task UploadVideoFile(string filePath, string blobPath, string fileNameOnBlob, string fileType = "mp4")
+        {
+            var data = File.ReadAllBytes(filePath);
+            var contentType = $"video/{fileType}";
+            
+            await UploadFile(OnPutBlobResponse, data, blobPath, fileNameOnBlob, contentType);
+        }
+        
+        public async Task UploadAudioFile(string filePath, string blobPath, string fileNameOnBlob, string fileType = "wav")
+        {
+            var data = File.ReadAllBytes(filePath);
+            var contentType = $"audio/{fileType}";
+            
+            await UploadFile(OnPutBlobResponse, data, blobPath, fileNameOnBlob, contentType);
+        }
+        
+        public async Task UploadImageFile(string filePath, string blobPath, string fileNameOnBlob, string fileType = "png")
+        {
+            var data = File.ReadAllBytes(filePath);
+            var contentType = $"image/{fileType}";
+            
+            await UploadFile(OnPutBlobResponse, data, blobPath, fileNameOnBlob, contentType);
+        }
+        
+        public async Task UploadText(string filePath, string blobPath, string fileNameOnBlob)
+        {
+            var data = File.ReadAllBytes(filePath);
+            var contentType = "text/plain; charset=UTF-8";
+            
+            await UploadFile(OnPutBlobResponse, data, blobPath, fileNameOnBlob, contentType);
+        }
+        
+        public async Task UploadAssetBundle(string filePath, string blobPath, string fileNameOnBlob)
+        {
+            var data = File.ReadAllBytes(filePath);
+            var contentType = "application/octet-stream";
+            
+            await UploadFile(OnPutBlobResponse, data, blobPath, fileNameOnBlob, contentType);
+        }
+        
+        public async Task UploadFile(Action<RestResponse> responseAction, byte[] data, string blobPath, string fileNameOnBlob, string contentType)
+        {
+            var convertedName = Uri.EscapeUriString(fileNameOnBlob);
+            
+            responseAction += OnPutBlobResponse;
+
+            var putRoutine = blobService.PutBlob(responseAction, data, blobPath, convertedName, contentType);
+
+            await putRoutine;
+        }
+
+        #endregion
+        
+        #region Download
+        
+        public async Task DownloadVideoFile(Action<IRestResponse<byte[]>> responseAction, string blobFilePath)
+        {
+            responseAction += OnGetBlobResponse;
+
+            var getRoutine = blobService.GetBlob(responseAction, Uri.EscapeUriString(blobFilePath));
+
+            await getRoutine;
+        }
+        
+        public async Task DownloadAudioFile(Action<IRestResponse<AudioClip>> responseAction, string blobFilePath)
+        {
+            responseAction += OnGetBlobResponse;
+
+            var getRoutine = blobService.GetAudioBlob(responseAction, Uri.EscapeUriString(blobFilePath));
+
+            await getRoutine;
+        }
+        
+        public async Task DownloadImageFile(Action<IRestResponse<Texture>> responseAction, string blobFilePath)
+        {
+            responseAction += OnGetBlobResponse;
+
+            var getRoutine = blobService.GetImageBlob(responseAction, Uri.EscapeUriString(blobFilePath));
+
+            await getRoutine;
+        }
+        
+        public async Task DownloadTextFile(Action<RestResponse> responseAction, string blobFilePath)
+        {
+            responseAction += OnGetBlobResponse;
+
+            var getRoutine = blobService.GetTextBlob(responseAction, Uri.EscapeUriString(blobFilePath));
+
+            await getRoutine;
+        }
+        
+        public async Task DownloadAssetBundle(Action<IRestResponse<AssetBundle>> responseAction, string blobFilePath)
+        {
+            responseAction += OnGetBlobResponse;
+
+            var getRoutine = blobService.GetAssetBundle(responseAction, Uri.EscapeUriString(blobFilePath));
+
+            await getRoutine;
+        }
+
+        public async Task DownloadBlob(Action<IRestResponse<byte[]>> responseAction, string blobFilePath)
+        {
+            responseAction += OnGetBlobResponse;
+
+            var getRoutine = blobService.GetBlob(responseAction, Uri.EscapeUriString(blobFilePath));
+
+            await getRoutine;
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async void DeleteFile(string blobPath, string fileName)
+        {
+            await DeleteBlob(delegate {  }, blobPath, fileName);
+        }
+
+        private async Task DeleteBlob(Action<RestResponse> response, string blobPath, string fileName)
+        {
+            response += OnDeleteBlobResponse;
+            await blobService.DeleteBlob(response, blobPath, Uri.EscapeUriString(fileName));
+        }
+
+        #endregion
+        
+        #region Callback
+
+        private void OnPutBlobResponse(RestResponse response)
+        {
+            Debug.Log($"애저 Blob 업로드: {!response.IsError} / {response.ErrorMessage} / {response.Url}");
+
+            HandleError(response.ErrorMessage);
+        }
+
+        private void OnGetBlobResponse<T>(IRestResponse<T> response)
+        {
+            Debug.Log($"애저 Blob 다운로드: {!response.IsError} / {response.ErrorMessage} / {response.Url}");
+            
+            HandleError(response.ErrorMessage);
+        }
+
+        private void OnGetBlobResponse(RestResponse response)
+        {
+            Debug.Log($"애저 Blob 다운로드: {!response.IsError} / {response.ErrorMessage} / {response.Url}");
+            
+            HandleError(response.ErrorMessage);
+        }
+        
+        private void OnDeleteBlobResponse(RestResponse response)
+        {
+            Debug.Log($"애저 Blob 삭제: {!response.IsError} / {response.ErrorMessage} / {response.Url}");
+
+            HandleError(response.ErrorMessage);
+        }
+
+        private void HandleError(string errorMessage)
+        {
+            if (errorMessage == string.Empty) return;
+
+            if (errorMessage.Contains("RequestEntityTooLarge"))
+            {
+                Debug.LogWarning(
+                    "Azure Blob Storage에 업로드 용량 제한이 걸려있습니다.\n" +
+                    "버전 2016-05-31 ~ 버전 2019-07-07에서는 일반적으로 256Mb 이상의 데이터는 업로드 불가합니다.\n" +
+                    "업로드를 원한다면 (스토리지 계정->설정->구성->대용량 파일 공유)를 체크하세요.");
+            }
+        }
+
+        #endregion
+
+        #region Demo
+
+        private async void DownloadFileDemo()
+        {
+            var blobFilePath = $"{tempPath}/{tempFileName}";
+
+            await DownloadVideoFile(delegate(IRestResponse<byte[]> response)
+            {
+                if (!response.IsError)
+                {
+                    var path = $"{Application.persistentDataPath}/{tempPath}/";
+
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    File.WriteAllBytes($"{path + tempFileName}", response.Data);
+                }
+            }, blobFilePath);
+        }
+        
+        private async void DeleteVideoFileDemo()
+        {
+            await DeleteBlob(delegate {  }, tempPath, tempFileName);
+        }
+
+        #endregion
+    }
+}
--- Assets/BKK/Scripts/FilePicker/FilePicker.cs
+++ Assets/BKK/Scripts/FilePicker/FilePicker.cs
@@ -1,150 +1,217 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-namespace BKK.Utility
+/// <summary>
+/// 모바일 파일 폴더에 접근하여 선택한 파일의 경로를 가져올 수 있게 해줍니다.
+///
+/// 변고경
+/// </summary>
+public class FilePicker : MonoBehaviour
 {
-    /// <summary>
-    /// 모바일 파일 폴더에 접근하여 선택한 파일의 경로를 가져올 수 있게 해줍니다.
-    ///
-    /// 변고경
-    /// </summary>
-    public class FilePicker : MonoBehaviour
+    [SerializeField] private List<string> fileTypeList = new List<string> {"pdf"};
+    private List<string> fileMIMEList = new List<string>();
+    private List<string> filePathList = new List<string>();
+
+    private string[] documentFileTypes = {"pdf"};
+    private string[] videoFileTypes = {"mp4", "avi", "mkv", "mov"};
+    private string[] audioFileTypes = {"wav", "mp3"};
+    private string[] imageFileTypes = {"jpeg", "jpg", "png"};
+
+    private bool completed = false;
+
+    public bool canMultipleSelect = false;
+    
+    [SerializeField] private bool debug = false;
+    
+    private void Start()
     {
-        [SerializeField] private List<string> fileTypeList = new List<string>() {"pdf"};
-        private List<string> fileMIMEList = new List<string>();
-        private List<string> filePathList = new List<string>();
+        ConvertExtensionToFileTypeList(fileTypeList.ToArray());
+    }
 
-        private bool completed = false;
-
-        public bool canMultipleSelect = false;
-
-        private void Start()
+    private void OnGUI()
+    {
+        if(!debug) return;
+        
+#if UNITY_EDITOR
+        if (GUI.Button(new Rect(Screen.width - 150, 50, 100, 40), "Pick PDF File"))
         {
-            ConvertExtensionToFileTypeList(fileTypeList.ToArray());
-        }
-
-        /// <summary>
-        /// PDF 파일을 가져와 WebviewController를 통해 웹뷰에 표시한다.
-        /// </summary>
-        public void PickPdfFile(Action<string[]> afterAction)
-        {
-            PickFile(new[] {"pdf"}, afterAction);
-        }
-
-        /// <summary>
-        /// 비디오 파일을 가져온다.
-        /// </summary>
-        /// <param name="afterAction"></param>
-        public void PickVideoFile(Action<string[]> afterAction)
-        {
-            PickFile(new[] {"mp4", "avi", "mkv", "mov"}, afterAction);
-        }
-
-        /// <summary>
-        /// 폴더 내 선택한 파일 경로 가져오기.
-        /// FilePicker 컴포넌트의 인스펙터 변수를 사용.
-        /// </summary>
-        /// <param name="afterAction"></param>
-        public void PickFile(Action<string[]> afterAction)
-        {
-            PickFile(fileTypeList.ToArray(), afterAction);
-        }
-
-        /// <summary>
-        /// 폴더 내 선택한 파일 경로 가져오기.
-        /// </summary>
-        /// <param name="fileTypes">파일 포맷.</param>
-        /// <param name="afterAction">가져온 파일 경로로 실행할 이벤트</param>
-        public async void PickFile(string[] fileTypes, Action<string[]> afterAction)
-        {
-            ConvertExtensionToFileTypeList(fileTypes);
-
-            PickPath(canMultipleSelect);
-
-            await UniTask.WaitUntil(() => completed);
-
-            afterAction?.Invoke(filePathList.ToArray());
-        }
-
-        /// <summary>
-        /// 파일 폴더를 열고 선택한 파일들의 경로를 가져온다.
-        /// </summary>
-        /// <param name="multipleSelect">다중 선택 여부. 모바일만 사용 가능</param>
-        public void PickPath(bool multipleSelect = false)
-        {
-            completed = false;
-
-            if (multipleSelect)
+            PickDocumentFile(delegate(string[] pathList)
             {
-                if (!NativeFilePicker.CanPickMultipleFiles())
-                {
-                    Debug.LogWarning("파일 다중 선택은 PC 및 에디터에서 지원하지 않습니다.");
-                    return;
-                }
+                var pdfPath = pathList.First(t => t.EndsWith("pdf", StringComparison.OrdinalIgnoreCase));
+                LoadPdf(pdfPath);
+            });
+        }
+        
+        if (GUI.Button(new Rect(Screen.width - 150, 100, 100, 40), "Pick Video File"))
+        {
+            PickVideoFile(delegate(string[] pathList)
+            {
+                var videoPath = pathList.First();
+                LoadVideo(videoPath);
+            });
+        }
+#endif
+    }
 
-                NativeFilePicker.PickMultipleFiles(delegate(string[] paths)
-                {
-                    filePathList.Clear();
-                    SetMultiplePaths(paths);
-                    completed = true;
-                }, fileMIMEList.ToArray());
+    /// <summary>
+    ///문서 파일을 가져온다.
+    /// </summary>
+    /// <param name="afterAction"></param>
+    public void PickDocumentFile(Action<string[]> afterAction)
+    {
+        PickFile(documentFileTypes, afterAction);
+    }
+    
+    /// <summary>
+    /// 비디오 파일을 가져온다.
+    /// </summary>
+    /// <param name="afterAction"></param>
+    public void PickVideoFile(Action<string[]> afterAction)
+    {
+        PickFile(videoFileTypes, afterAction);
+    }
+    
+    /// <summary>
+    /// 오디오 파일을 가져온다.
+    /// </summary>
+    /// <param name="afterAction"></param>
+    public void PickAudioFile(Action<string[]> afterAction)
+    {
+        PickFile(audioFileTypes, afterAction);
+    }
+    
+    /// <summary>
+    /// 이미지 파일을 가져온다.
+    /// </summary>
+    /// <param name="afterAction"></param>
+    public void PickImageFile(Action<string[]> afterAction)
+    {
+        PickFile(imageFileTypes, afterAction);
+    }
+
+    /// <summary>
+    /// 폴더 내 선택한 파일 경로 가져오기.
+    /// FilePicker 컴포넌트의 인스펙터 변수를 사용.
+    /// </summary>
+    /// <param name="afterAction"></param>
+    public void PickFile(Action<string[]> afterAction)
+    {
+        PickFile(fileTypeList.ToArray(), afterAction);
+    }
+    
+    /// <summary>
+    /// 폴더 내 선택한 파일 경로 가져오기.
+    /// </summary>
+    /// <param name="fileTypes">파일 포맷.</param>
+    /// <param name="afterAction">가져온 파일 경로로 실행할 이벤트</param>
+    public async void PickFile(string[] fileTypes, Action<string[]> afterAction)
+    {
+        ConvertExtensionToFileTypeList(fileTypes);
+        
+        PickPath(canMultipleSelect);
+
+        await UniTask.WaitUntil(() => completed);
+
+        afterAction?.Invoke(filePathList.ToArray());
+    }
+
+    /// <summary>
+    /// 파일 폴더를 열고 선택한 파일들의 경로를 가져온다.
+    /// </summary>
+    /// <param name="multipleSelect">다중 선택 여부. 모바일만 사용 가능</param>
+    private void PickPath(bool multipleSelect = false)
+    {
+        completed = false;
+
+        if (multipleSelect)
+        {
+            if (!NativeFilePicker.CanPickMultipleFiles())
+            {
+                Debug.LogWarning("파일 다중 선택은 PC 및 에디터에서 지원하지 않습니다.");
+                return;
             }
-            else
+            
+            NativeFilePicker.PickMultipleFiles(delegate(string[] paths)
             {
-                NativeFilePicker.PickFile(delegate(string path)
-                {
-                    filePathList.Clear();
-                    SetPath(path);
-                    completed = true;
-                }, fileMIMEList.ToArray());
-            }
+                filePathList.Clear();
+                SetMultiplePaths(paths);
+                completed = true;
+            }, fileMIMEList.ToArray());
         }
-
-        /// <summary>
-        /// 가져온 파일 경로를 리스트에 등록한다.
-        /// </summary>
-        /// <param name="path">파일 경로</param>
-        private void SetPath(string path)
+        else
         {
-            if (path == null)
+            NativeFilePicker.PickFile(delegate(string path)
             {
-                Debug.Log("파일 가져오기를 취소합니다.");
-            }
-            else
-            {
-                Debug.Log($"가져온 파일: {path}");
-                filePathList.Add(path);
-            }
-        }
-
-        /// <summary>
-        /// 가져온 여러개의 파일 경로를 리스트에 등록한다.
-        /// </summary>
-        /// <param name="paths">파일 경로들</param>
-        private void SetMultiplePaths(string[] paths)
-        {
-            foreach (var path in paths)
-            {
+                filePathList.Clear();
                 SetPath(path);
-            }
+                completed = true;
+            }, fileMIMEList.ToArray());
         }
+    }
 
-        /// <summary>
-        /// 파일 타입을 MIME/UTI로 변환한다.
-        /// </summary>
-        /// <param name="fileTypes">파일 포맷들</param>
-        public void ConvertExtensionToFileTypeList(string[] fileTypes)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="pdfPath"></param>
+    private void LoadPdf(string pdfPath)
+    {
+        
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="videoPath"></param>
+    private void LoadVideo(string videoPath)
+    {
+        
+    }
+
+    /// <summary>
+    /// 가져온 파일 경로를 리스트에 등록한다.
+    /// </summary>
+    /// <param name="path">파일 경로</param>
+    private void SetPath(string path)
+    {
+        if (path == null)
         {
-            fileMIMEList.Clear();
-            foreach (var fileType in fileTypes)
-            {
-                var extension = NativeFilePicker.ConvertExtensionToFileType(fileType);
-                fileMIMEList.Add(extension);
+            Debug.Log("파일 가져오기를 취소합니다.");
+        }
+        else
+        {
+            Debug.Log($"가져온 파일: {path}");
+            filePathList.Add(path);
+        }
+    }
 
-                Debug.Log($"{fileType}의 MIME/UTI: {extension}");
-            }
+    /// <summary>
+    /// 가져온 여러개의 파일 경로를 리스트에 등록한다.
+    /// </summary>
+    /// <param name="paths">파일 경로들</param>
+    private void SetMultiplePaths(string[] paths)
+    {
+        foreach (var path in paths)
+        {
+            SetPath(path);
+        }
+    }
+
+    /// <summary>
+    /// 파일 타입을 MIME/UTI로 변환한다.
+    /// </summary>
+    /// <param name="fileTypes">파일 포맷들</param>
+    private void ConvertExtensionToFileTypeList(string[] fileTypes)
+    {
+        fileMIMEList.Clear();
+        foreach (var fileType in fileTypes)
+        {
+            var extension = NativeFilePicker.ConvertExtensionToFileType(fileType);
+            fileMIMEList.Add(extension);
+
+            Debug.Log($"{fileType}의 MIME/UTI: {extension}");
         }
     }
 }
