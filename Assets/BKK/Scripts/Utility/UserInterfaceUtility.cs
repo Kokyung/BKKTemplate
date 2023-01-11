0619b39cfa18567e614e2f274a29646cda1ba9ac
Kokyung <43735316+Kokyung@users.noreply.github.com>
2023-01-11 14:27:33 +0900

    UIUtility Select 기능 추가

--- Assets/BKK/Scripts/Utility/UserInterfaceUtility.cs
+++ Assets/BKK/Scripts/Utility/UserInterfaceUtility.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace BKK.Utility
 {
@@ -8,10 +9,11 @@ namespace BKK.Utility
     {
         public static bool IsPointerOverUI(int layer = 5)
         {
-            return IsPointerOverUIElement(GetEventSystemRaycastResults(), layer);
+            var raycastResult = GetEventSystemRaycastResults(Input.mousePosition);
+            return IsPointerOverUIElement(raycastResult, layer);
         }
 
-        private static bool IsPointerOverUIElement(IReadOnlyList<RaycastResult> eventSystemRaycastResults,
+        public static bool IsPointerOverUIElement(IReadOnlyList<RaycastResult> eventSystemRaycastResults,
             int layer = 5)
         {
             foreach (var curRaycastResult in eventSystemRaycastResults)
@@ -21,16 +23,143 @@ namespace BKK.Utility
 
             return false;
         }
-
-        private static List<RaycastResult> GetEventSystemRaycastResults()
+        
+        public static List<RaycastResult> GetEventSystemRaycastResults(Vector2 pos)
         {
             var eventData = new PointerEventData(EventSystem.current);
             var raycastResults = new List<RaycastResult>();
 
-            eventData.position = Input.mousePosition;
+            eventData.position = pos;
             EventSystem.current.RaycastAll(eventData, raycastResults);
             return raycastResults;
         }
+
+        #region Select
+
+        public static void SelectDown<T>() where T : Selectable
+        {
+            var currentSelected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+            SelectDown<T>(currentSelected);
+        }
+
+        public static void SelectUp<T>() where T : Selectable
+        {
+            var currentSelected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+            SelectUp<T>(currentSelected);
+        }
+
+        public static void SelectDown<T1, T2>()
+            where T1 : Selectable
+            where T2 : Selectable
+        {
+            var currentSelected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+            SelectDown<T1, T2>(currentSelected);
+        }
+
+        public static void SelectUp<T1, T2>()
+            where T1 : Selectable
+            where T2 : Selectable
+        {
+            var currentSelected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+            SelectUp<T1,T2>(currentSelected);
+        }
+
+        public static void SelectDown<T1, T2, T3>()
+            where T1 : Selectable
+            where T2 : Selectable
+            where T3 : Selectable
+        {
+            var currentSelected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+            SelectDown<T1, T2, T3>(currentSelected);
+        }
+
+        public static void SelectUp<T1, T2, T3>()
+            where T1 : Selectable
+            where T2 : Selectable
+            where T3 : Selectable
+        {
+            var currentSelected = EventSystem.current.currentSelectedGameObject.GetComponent<Selectable>();
+            SelectUp<T1,T2, T3>(currentSelected);
+        }
+        
+        public static void SelectDown<T>(Selectable selectable) where T : Selectable
+        {
+            var next = selectable.FindSelectableOnDown();
+            var isValid = next is T;
+
+            if (next != null && isValid)
+            {
+                next.Select();
+            }
+        }
+
+        public static void SelectUp<T>(Selectable selectable) where T : Selectable
+        {
+            var next = selectable.FindSelectableOnUp();
+            var isValid = next is T;
+
+            if (next != null && isValid)
+            {
+                next.Select();
+            }
+        }
+
+        public static void SelectDown<T1, T2>(Selectable selectable)
+            where T1 : Selectable
+            where T2 : Selectable
+        {
+            var next = selectable.FindSelectableOnDown();
+            var isValid = next is T1 || next is T2;
+
+            if (next != null && isValid)
+            {
+                next.Select();
+            }
+        }
+
+        public static void SelectUp<T1, T2>(Selectable selectable)
+            where T1 : Selectable
+            where T2 : Selectable
+        {
+            var next = selectable.FindSelectableOnUp();
+            var isValid = next is T1 || next is T2;
+
+            if (next != null && isValid)
+            {
+                next.Select();
+            }
+        }
+
+        public static void SelectDown<T1, T2, T3>(Selectable selectable)
+            where T1 : Selectable
+            where T2 : Selectable
+            where T3 : Selectable
+        {
+            var next = selectable.FindSelectableOnDown();
+            var isValid = next is T1 || next is T2 || next is T3;
+
+            if (next != null && isValid)
+            {
+                next.Select();
+            }
+        }
+
+        public static void SelectUp<T1, T2, T3>(Selectable selectable)
+            where T1 : Selectable
+            where T2 : Selectable
+            where T3 : Selectable
+        {
+            var next = selectable.FindSelectableOnUp();
+            var isValid = next is T1 || next is T2 || next is T3;
+
+            if (next != null && isValid)
+            {
+                next.Select();
+            }
+        }
+
+        #endregion
+        
     }
 }
 
